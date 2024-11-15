aa9fd08ac2b0b716cce72a52b605145981700ecf
Lucemans <luc@lucemans.nl>
2024-11-15 07:21:33 +0100

    Update eslint & queryclient

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
-import { useMutation } from '@tanstack/react-query';
 import './global.css';
+
+import { useMutation } from '@tanstack/react-query';
 
 export const BACKEND_URL = 'https://poap-distributor.c5.v3x.systems';
 
@@ -7,20 +8,25 @@ export const App = () => {
     const { mutate } = useMutation({
         mutationFn: async () => {
             // fetch poap from backend url /poap and if successfull redirect user to link inside {data: 'https://...'}
-            const res = await fetch(`${BACKEND_URL}/poap`);
-            const data = await res.json();
-            if (res.ok) {
+            const response = await fetch(`${BACKEND_URL}/poap`);
+            const data = await response.json();
+
+            if (response.ok) {
+                // eslint-disable-next-line no-undef
                 window.location.href = data.data;
             }
-        }
-    })
+        },
+    });
 
     return (
         <div className="w-full min-h-screen bg-ens-light-background-secondary pt-12">
             <div className="p-4 border-ens-light-border text-center bg-ens-light-background-primary text-ens-light-text-primary mx-auto max-w-xl w-full border rounded-xl space-y-4">
                 <h1 className="font-bold text-2xl">Claim Music Stage POAP</h1>
                 <p>Click the button below to claim your POAP.</p>
-                <button className="bg-ens-light-blue-primary w-full text-ens-light-background-primary px-4 py-2 rounded-md block" onClick={() => mutate()}>
+                <button
+                    className="bg-ens-light-blue-primary w-full text-ens-light-background-primary px-4 py-2 rounded-md block"
+                    onClick={() => mutate()}
+                >
                     Claim POAP
                 </button>
             </div>
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { App } from './App';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+const queryClient = new QueryClient();
+const root = createRoot(document.querySelector('#root') as HTMLElement);
+
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+);
